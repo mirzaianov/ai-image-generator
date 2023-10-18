91ec56d493d571458b835538d535cf5e76fd1504
msejak <7o.mseJak@gmail.com>
2023-10-18 18:58:01 +0300

    fate: layout correction Started working mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,79 @@
-import { ImageGenerator } from './components/ImageGenerator/ImageGenerator';
+import { useRef, useState } from 'react';
+import Spinner from './Spinner.jsx';
+
+import defaultImage from '/default-image.svg';
 
 import './App.scss';
 
+const API_URL = 'https://api.openai.com/v1/images/generations';
+
 function App() {
+  const [imageUrl, setImageUrl] = useState('/');
+  const [loading, setLoading] = useState(false);
+
+  const inputRef = useRef(null);
+
+  const imageGenerator = async () => {
+    if (inputRef.current.value === '') {
+      return 0;
+    }
+
+    setLoading(true);
+
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        'User-Agent': 'Chrome',
+      },
+      body: JSON.stringify({
+        prompt: inputRef.current.value,
+        n: 1,
+        size: '512x512',
+      }),
+    });
+
+    const data = await response.json();
+    const dataArray = data.data;
+
+    setImageUrl(dataArray[0].url);
+
+    setLoading(false);
+  };
+
   return (
-    <div className="app">
-      <ImageGenerator />
+    <div className="app container">
+      <div className="header">
+        <span className="header--main">AI Image</span>
+        <span className="header--accent">Generator</span>
+      </div>
+      <div className="img-loading">
+        <div className="image">
+          <img
+            src={imageUrl === '/' ? defaultImage : imageUrl}
+            alt="Girl"
+          />
+        </div>
+        <div className="spinner">{loading && <Spinner />}</div>
+      </div>
+      <div className="search-box">
+        <input
+          type="text"
+          ref={inputRef}
+          className="search-input"
+          placeholder="Describe What You Want to See"
+        />
+        <div className="btns">
+          <button
+            className="btn"
+            onClick={() => imageGenerator()}
+          >
+            Generate
+          </button>
+          <button className="btn btn--download">Download</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Spinner.jsx
+++ src/Spinner.jsx
@@ -1,4 +1,12 @@
 export default function Spinner() {
+  const saturation = 50;
+  const circle1 = `hsl(326, 78%, ${saturation}%)`;
+  const circle2 = `hsl(326, 78%, ${saturation + 10}%)`;
+  const circle3 = `hsl(326, 78%, ${saturation + 20}%)`;
+  const circle4 = `hsl(326, 78%, ${saturation + 30}%)`;
+  const circle5 = `hsl(326, 78%, ${saturation + 40}%)`;
+  const circle6 = `hsl(326, 78%, ${saturation + 45}%)`;
+
   return (
     <svg
       xmlns:svg="http://www.w3.org/2000/svg"
@@ -15,55 +23,55 @@ export default function Spinner() {
           cx="16"
           cy="64"
           r="16"
-          fill="rgb(222, 27, 137)"
+          fill={circle1}
         />
         <circle
           cx="16"
           cy="64"
           r="16"
-          fill="rgb(233, 103, 176)"
+          fill={circle2}
           transform="rotate(45,64,64)"
         />
         <circle
           cx="16"
           cy="64"
           r="16"
-          fill="rgb(241, 159, 205)"
+          fill={circle3}
           transform="rotate(90,64,64)"
         />
         <circle
           cx="16"
           cy="64"
           r="16"
-          fill="#f8d1e7"
+          fill={circle4}
           transform="rotate(135,64,64)"
         />
         <circle
           cx="16"
           cy="64"
           r="16"
-          fill="#fbe4f1"
+          fill={circle5}
           transform="rotate(180,64,64)"
         />
         <circle
           cx="16"
           cy="64"
           r="16"
-          fill="#fbe4f1"
+          fill={circle6}
           transform="rotate(225,64,64)"
         />
         <circle
           cx="16"
           cy="64"
           r="16"
-          fill="#fbe4f1"
+          fill={circle6}
           transform="rotate(270,64,64)"
         />
         <circle
           cx="16"
           cy="64"
           r="16"
-          fill="#fbe4f1"
+          fill={circle6}
           transform="rotate(315,64,64)"
         />
         <animateTransform
