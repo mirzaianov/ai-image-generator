508e1ec4d4b75f1740ab5db755daffefd1a22fdb
msejak <7o.mseJak@gmail.com>
2023-10-23 16:41:54 +0300

    fix: form Changed div to form

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,22 @@ import './App.scss';
 const API_URL = 'https://api.openai.com/v1/images/generations';
 
 function App() {
+  console.log('render');
+
   const [imageUrl, setImageUrl] = useState('/');
   const [loading, setLoading] = useState(false);
 
   const inputRef = useRef(null);
 
-  // const resetRef = () => {
-  //   inputRef.current.value = '';
-  // };
+  const imageGenerator = async () => {
+    console.log('imageGenerator');
+    if (inputRef.current.value === '') {
+      return 0;
+    }
 
-  const fetchData = async () => {
-    return await fetch(API_URL, {
+    setLoading(true);
+
+    const response = await fetch(API_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -32,36 +37,21 @@ function App() {
         size: '512x512',
       }),
     });
-  };
 
-  const imageGenerator = async () => {
-    if (inputRef.current.value === '') {
-      return 0;
-    }
-
-    setLoading(true);
-
-    const response = await fetchData();
     const data = await response.json();
     const dataArray = data.data;
 
     setImageUrl(dataArray[0].url);
-    // resetRef();
     setLoading(false);
   };
 
   const handleImageDownload = () => {
+    console.log('handleImageDownload');
     if (imageUrl === '/') {
       return 0;
     }
 
     saveAs(imageUrl, 'image.png');
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      imageGenerator();
-    }
   };
 
   return (
@@ -79,9 +69,12 @@ function App() {
         </div>
         <div className="spinner">{loading && <Spinner />}</div>
       </div>
-      <div
+      <form
         className="search-box"
-        // action="#"
+        onSubmit={(e) => {
+          e.preventDefault();
+          imageGenerator();
+        }}
       >
         <input
           required
@@ -89,15 +82,15 @@ function App() {
           ref={inputRef}
           className="search-input"
           placeholder="Describe What You Want to See"
-          onKeyDown={handleKeyDown}
         />
         <div className="btns">
-          <input
+          <button
             className="btn"
             type="submit"
             value="Generate"
-            onClick={() => imageGenerator()}
-          />
+          >
+            Generate
+          </button>
           <input
             className="btn btn--download"
             type="button"
@@ -105,7 +98,7 @@ function App() {
             onClick={() => handleImageDownload(imageUrl)}
           />
         </div>
-      </div>
+      </form>
     </div>
   );
 }
