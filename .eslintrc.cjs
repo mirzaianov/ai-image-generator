446252a87f0168a30c909e69c7ef6209126d554e
msejak <7o.mseJak@gmail.com>
2023-11-07 12:41:11 +0300

    fate: added radius Added radius to the rendered image

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,9 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    // 'eslint:recommended',
     'airbnb-base',
+    // 'prettier',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
